9c09b234c10e206e0a4f67d70238430527e29e51
ryzenboi98 <joaopro23@gmail.com>
2021-03-15 14:06:45 +0000

    Added first and last name field. Changed the username and email validation to unique values.

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -2,12 +2,15 @@ from django.contrib.auth.models import Group
 from rest_framework import serializers
 from django.contrib.auth import get_user_model,password_validation
 from django.contrib.auth.hashers import make_password
+from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    username = serializers.CharField(min_length=6, max_length=20)
-    email = serializers.EmailField(style={'input_type':'email'})
+    first_name = serializers.CharField(max_length=20)
+    last_name = serializers.CharField(max_length=20)
+    username = serializers.CharField(min_length=6, max_length=20, validators=[UniqueValidator(queryset=User.objects.all())])
+    email = serializers.EmailField(style={'input_type':'email'}, validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(style={'input_type':'password'}, write_only=True)
 
     def validate_password(self, value):
@@ -18,9 +21,11 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'password']
+        fields = ['url', 'first_name', 'last_name', 'username', 'email', 'password']
 
     def create(self, validated_data):
+        validated_data['first_name'] = validated_data['first_name'].capitalize()
+        validated_data['last_name'] = validated_data['last_name'].capitalize()
         validated_data['password'] = make_password(validated_data['password'])
         return super(UserSerializer, self).create(validated_data)
 
